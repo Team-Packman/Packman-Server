51ee08829f3989aabda0d5cf3cccc635b3762c74
dingding-21 <phj8221@naver.com>
2022-07-16 14:55:46 +0900

    [ADD]/#3 함께패킹리스트. 그룹 collection 미리 생성

--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -1,17 +1,27 @@
-import mongoose from "mongoose";
-import config from "../config";
+import mongoose from 'mongoose';
+import config from '../config';
+import TogetherPackingList from '../models/TogetherPackingList';
+import Group from '../models/Group';
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(config.mongoURI);
+  try {
+    await mongoose.connect(config.mongoURI);
 
-        mongoose.set('autoCreate', true);
+    mongoose.set('autoCreate', true);
 
-        console.log("Mongoose Connected ...");
-    } catch (err: any) {
-        console.error(err.message);
-        process.exit(1);
-    }
+    console.log('Mongoose Connected ...');
+
+    TogetherPackingList.createCollection().then((collection) => {
+      console.log('TogetherPackingList Collection Created');
+    });
+
+    Group.createCollection().then((collection) => {
+      console.log('Group Collection Created');
+    });
+  } catch (err: any) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
