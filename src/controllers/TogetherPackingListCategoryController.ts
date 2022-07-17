ef3d42f18e7c583437a7a3a72a8473fb0e1b3106
dingding-21 <phj8221@naver.com>
2022-07-17 18:08:30 +0900

    [FEAT]/#22 함께 패킹리스트 카테고리 삭제 구현

--- src/controllers/TogetherPackingListCategoryController.ts
+++ src/controllers/TogetherPackingListCategoryController.ts
@@ -6,6 +6,9 @@ import { validationResult } from 'express-validator';
 import { TogetherPackingListCategoryCreateDto } from '../interface/ITogetherPackingList';
 import { TogetherPackingListCategoryService } from '../services';
 import { CategoryUpdateDto } from '../interface/ICategory';
+import { CategoryDeleteDto } from '../interface/ICategory';
+import Category from '../models/Category';
+import mongoose from 'mongoose';
 
 const createCategory = async (req: Request, res: Response) => {
   const error = validationResult(req);
@@ -28,10 +31,8 @@ const createCategory = async (req: Request, res: Response) => {
         .send(util.success(statusCode.BAD_REQUEST, message.NO_PACKINGLIST));
     } else {
       res
-        .status(statusCode.CREATED)
-        .send(
-          util.success(statusCode.CREATED, message.SUCCESS_CREATE_TOGETHER_CATEGORY_SUCCESS, data),
-        );
+        .status(statusCode.OK)
+        .send(util.success(statusCode.OK, message.SUCCESS_CREATE_TOGETHER_CATEGORY_SUCCESS, data));
     }
   } catch (error) {
     console.log(error);
@@ -68,10 +69,8 @@ const updateCategory = async (req: Request, res: Response) => {
         .send(util.fail(statusCode.BAD_REQUEST, message.NO_LIST_CATEGORY));
     } else {
       res
-        .status(statusCode.CREATED)
-        .send(
-          util.success(statusCode.CREATED, message.SUCCESS_UPDATE_TOGETHER_CATEGORY_SUCCESS, data),
-        );
+        .status(statusCode.OK)
+        .send(util.success(statusCode.OK, message.SUCCESS_UPDATE_TOGETHER_CATEGORY_SUCCESS, data));
     }
   } catch (error) {
     console.log(error);
@@ -80,7 +79,38 @@ const updateCategory = async (req: Request, res: Response) => {
       .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
   }
 };
+
+const deleteCategory = async (req: Request, res: Response) => {
+  const { listId, categoryId } = req.params;
+  try {
+    const data = await TogetherPackingListCategoryService.deleteCategory(listId, categoryId);
+    if (data === 'no_list') {
+      res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.NO_PACKINGLIST));
+    } else if (data === 'no_category') {
+      res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.NO_CATEGORY));
+    } else if (data === 'no_list_category') {
+      res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.NO_LIST_CATEGORY));
+    } else {
+      res
+        .status(statusCode.OK)
+        .send(util.success(statusCode.OK, message.SUCCESS_DELETE_TOGETHER_CATEGORY_SUCCESS, data));
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+  }
+};
+
 export default {
   createCategory,
   updateCategory,
+  deleteCategory,
 };
