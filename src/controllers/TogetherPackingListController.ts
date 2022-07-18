27176ca1ccf401c5683652213261aff6441f92c4
laalaa31 <jseoh4298@gmail.com>
2022-07-18 19:03:58 +0900

    [ADD] 파라미터 오류처리 추가

--- src/controllers/TogetherPackingListController.ts
+++ src/controllers/TogetherPackingListController.ts
@@ -90,6 +90,11 @@ const readTogetherPackingList = async (req: Request, res: Response) => {
 
 const deleteTogetherPackingList = async (req: Request, res: Response) => {
   const { folderId, listId } = req.params;
+  if (!folderId || !listId)
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, message.INVALID_PARAMS));
+
   try {
     const data = await TogetherPackingListService.deleteTogetherPackingList(folderId, listId);
 
@@ -112,4 +117,5 @@ const deleteTogetherPackingList = async (req: Request, res: Response) => {
 export default {
   createTogetherPackingList,
   readTogetherPackingList,
+  deleteTogetherPackingList,
 };
--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -3,6 +3,7 @@ const message = {
   NOT_FOUND: '존재하지 않는 자원',
   BAD_REQUEST: '잘못된 요청',
   INTERNAL_SERVER_ERROR: '서버 내부 오류',
+  INVALID_PARAMS: '유효하지 않은 파라미터 입니다',
 
   // 토큰 관련
   NULL_VALUE_TOKEN: '토큰이 없습니다',
