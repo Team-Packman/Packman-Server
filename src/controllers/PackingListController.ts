bbf85943a01a63ebb4383355624703f386c5141d
laalaa31 <jseoh4298@gmail.com>
2022-07-18 14:58:01 +0900

    [FEAT]/#34 패킹리스트 나의 템플릿 여부  수정 구현

--- src/controllers/PackingListController.ts
+++ src/controllers/PackingListController.ts
@@ -3,7 +3,11 @@ import statusCode from '../modules/statusCode';
 import message from '../modules/responseMessage';
 import util from '../modules/util';
 import { validationResult } from 'express-validator/check';
-import { PackingListDateUpdateDTO, PackingListTitleUpdateDTO } from '../interface/IPackingList';
+import {
+  PackingListDateUpdateDTO,
+  PackingListMyTemplateUpdateDTO,
+  PackingListTitleUpdateDTO,
+} from '../interface/IPackingList';
 import PackingListService from '../services/PackingListService';
 
 /**
@@ -83,7 +87,45 @@ const updatePackingListDate = async (req: Request, res: Response) => {
       .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
   }
 };
+
+/**
+ *  @route PATCH /packingList/myTemplate
+ *  @desc Update Packinglist Is Saved
+ *  @access Public
+ **/
+
+const updatePackingListMyTemplate = async (req: Request, res: Response) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
+  }
+
+  const packingListMyTemplateUpdateDto: PackingListMyTemplateUpdateDTO = req.body;
+
+  try {
+    const data = await PackingListService.updatePackingListMyTemplate(
+      packingListMyTemplateUpdateDto,
+    );
+
+    if (data == 'notfoundUpdatedMyTemplate')
+      res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.NO_UPDATEDMYTEMPLATE));
+    else
+      res
+        .status(statusCode.OK)
+        .send(util.success(statusCode.OK, message.UPDATE_PACKINGLIST_MYTEMPLATE_SUCCESS, data));
+  } catch (error) {
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+  }
+};
 export default {
   updatePackingListTitle,
   updatePackingListDate,
+  updatePackingListMyTemplate,
 };
