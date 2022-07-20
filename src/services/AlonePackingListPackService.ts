cc00f73b7e1c4473932816adafb38694ad0afa5d
kkl4846 <kkl4846@cau.ac.kr>
2022-07-20 20:28:14 +0900

    [FEAT]/#85 혼자 패킹리스트 짐 수정

--- src/services/AlonePackingListPackService.ts
+++ src/services/AlonePackingListPackService.ts
@@ -1,4 +1,4 @@
-import { PackCreateDto } from '../interface/IPack';
+import { PackCreateDto, PackUpdateDto } from '../interface/IPack';
 import Pack from '../models/Pack';
 import Category from '../models/Category';
 
@@ -65,6 +65,72 @@ const createPack = async (
   }
 };
 
+
+const updatePack = async (
+  packUpdateDto: PackUpdateDto,
+): Promise<AlonePackingListCategoryResponseDto | string> => {
+  try {
+    const packId = packUpdateDto._id;
+    const packName = packUpdateDto.name;
+    const isChecked = packUpdateDto.isChecked;
+    const listId = packUpdateDto.listId;
+    const categoryId = packUpdateDto.categoryId;
+
+    const pack = await Pack.findById(packId);
+    if (!pack) return 'no_pack';
+
+    const cate = await Category.findById(categoryId);
+    if (!cate) return 'no_category';
+
+    const list = await AlonePackingList.findById(listId);
+    if (!list) return 'no_list';
+
+    if (!list.category.includes(categoryId)) return 'no_list_category';
+    if (!cate.pack.includes(packId)) return 'no_category_pack';
+
+    await Pack.updateOne(
+      { _id: packId },
+      {
+        $set: {
+          name: packName,
+          isChecked: isChecked,
+        },
+      },
+    );
+
+    const data: AlonePackingListCategoryResponseDto | null = await AlonePackingList.findOne(
+      { _id: listId },
+      { category: 1 },
+    ).populate({
+      path: 'category',
+      model: 'Category',
+      select: { _id: 1, name: 1, pack: 1 },
+      options: { sort: { createdAt: 1 } },
+      populate: {
+        path: 'pack',
+        model: 'Pack',
+        select: { _id: 1, name: 1, isChecked: 1 },
+        options: { sort: { createdAt: 1 } },
+        populate: {
+          path: 'packer',
+          model: 'User',
+          select: {
+            _id: 1,
+            name: 1,
+          },
+        },
+      },
+    });
+    if (!data) return 'null';
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+
 export default {
   createPack,
+  updatePack
 };
