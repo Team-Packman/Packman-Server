76ced0c9d7d463b0d15d2db75d78a1dfc742bd04
laalaa31 <jseoh4298@gmail.com>
2022-07-21 05:17:43 +0900

    [FEAT]/#93 카카오 로그인 구현

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,11 +3,6 @@ import statusCode from '../modules/statusCode';
 import message from '../modules/responseMessage';
 import util from '../modules/util';
 import { AuthService } from '../services';
-import config from '../config';
-
-const GOOGLE_AUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
-const GOOGLE_AUTH_REDIRECT_URL = 'http://localhost:8000/auth/google/callback';
-
 
 /**
  *  @route GET /auth/google
@@ -38,8 +33,36 @@ const getGoogleUser = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ *  @route GET /auth/kakao
+ *  @desc get kakao user
+ *  @access public
+ **/
 
+const getKakaoUser = async (req: Request, res: Response) => {
+  const token = req.body.accessToken;
+  if (!token) {
+    res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE_TOKEN));
+  }
+  try {
+    const data = await AuthService.getKakaoUser(token);
+
+    if (!data)
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.INVALID_TOKEN));
+    res.status(statusCode.OK).send(util.success(statusCode.OK, message.SUCCESS_GET_TOKEN, data));
+  } catch (error) {
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(util.fail(statusCode.INTERNAL_SERVER_ERROR, message.INTERNAL_SERVER_ERROR));
+  }
+};
 
 export default {
   getGoogleUser,
+  getKakaoUser,
 };
--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import config from '../config';
 import { AuthResponseDto } from '../interface/IUser';
 import User from '../models/User';
 import getToken from '../modules/jwtHandler';
@@ -45,6 +44,47 @@ const getGoogleUser = async (googleToken: string): Promise<AuthResponseDto | nul
   }
 };
 
+const getKakaoUser = async (kakaoToken: string): Promise<AuthResponseDto | null | undefined> => {
+  try {
+    const response = await axios({
+      method: 'post',
+      url: 'https://kapi.kakao.com/v2/user/me',
+      headers: {
+        Authorization: `Bearer ${kakaoToken}`,
+      },
+    });
+
+    if (!response) return null;
+
+    // 존재하는 유저인지 판별
+    const userEmail = response.data.kakao_account.email;
+    const user = await User.findOne({ email: userEmail });
+    if (!user || user.isDeleted) {
+      if (user?.isDeleted) {
+        await User.findByIdAndDelete(user._id);
+      }
+      const data = {
+        isAlreadyUser: false,
+        email: userEmail,
+      };
+      return data;
+    } else {
+      const accessToken = getToken(user._id);
+      const data = {
+        isAlreadyUser: true,
+        token: accessToken,
+        _id: user._id,
+        email: userEmail,
+        name: user.name,
+        profileImageId: user.profileImageId,
+      };
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 export default {
   getGoogleUser,
+  getKakaoUser,
 };
