0c9a0f285e329fd28320256941ed4600119215ad
kkl4846 <kkl4846@cau.ac.kr>
2022-07-20 21:22:05 +0900

    [DEL] 필요없는 코드 삭제

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -42,69 +42,7 @@ const getGoogleUser = async (googleToken: string): Promise<AuthResponseDto | nul
   }
 };
 
-const GOOGLE_AUTH_TOKEN_URL = 'https://oauth2.googleapis.com/token';
-const GOOGLE_AUTH_REDIRECT_URL = 'http://localhost:8000/auth/google/callback';
-
-const getGoogleInfo = async (code: string): Promise<string | null> => {
-  try {
-    const { data } = await axios({
-      method: 'POST',
-      url: `${GOOGLE_AUTH_TOKEN_URL}`,
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
-      },
-      params: {
-        grant_type: 'authorization_code',
-        client_id: config.googleClientID,
-        client_secret: config.googleClientSECRET,
-        redirectUri: GOOGLE_AUTH_REDIRECT_URL,
-        code: code,
-      },
-    });
-
-    const access_token = data['access_token'];
-    if (!access_token) return null;
-    else {
-      const { data: userEmail } = await axios.get(
-        `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`,
-      );
-      const { email } = userEmail;
-      return email;
-    }
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
-
-const googleLogin = async (userEmail: string): Promise<AuthResponseDto> => {
-  try {
-    const user = await User.findOne({ email: userEmail });
-
-    if (!user) {
-      const data = {
-        isAlreadyUser: false,
-        email: userEmail,
-      };
-      return data;
-    } else {
-      const data = {
-        isAlreadyUser: true,
-        _id: user._id,
-        email: userEmail,
-        name: user.name,
-        profileImageId: user.profileImageId,
-      };
-      return data;
-    }
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
 
 export default {
-  getGoogleInfo,
-  googleLogin,
   getGoogleUser,
 };
